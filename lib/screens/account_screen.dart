f87f2e4dc4f0c296ebd7ec70a9ebd6969a0b794d
ipederson <ipederson@mpulsesoftware.com>
2023-08-29 16:02:13 -0700

    added routes for fav, search, & checkout screens

--- lib/screens/account_screen.dart
+++ lib/screens/account_screen.dart
@@ -1,7 +1,12 @@
 import 'package:fabrics/components/widgets/rounded_bottom_app_bar.dart';
+import 'package:fabrics/models/provider/app_bar_selected.dart';
+import 'package:fabrics/screens/search_screen.dart';
+import 'package:fabrics/screens/wishlist.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../components/widgets/detailed_card.dart';
+import 'checkout_screen.dart';
 
 class AccountScreen extends StatelessWidget {
   static String id = 'accountScreen';
@@ -17,27 +22,43 @@ class AccountScreen extends StatelessWidget {
           DetailedCard(
             icon: Icons.account_box_rounded,
             text: 'Settings',
-            onTap: () {},
+            onTap: () {
+              // Go to account page
+            },
           ),
           DetailedCard(
             icon: Icons.assignment_sharp,
             text: 'Orders',
-            onTap: () {},
+            onTap: () {
+              // Go to order history page
+            },
           ),
           DetailedCard(
             icon: Icons.favorite,
             text: 'Favorites',
-            onTap: () {},
+            onTap: () {
+              context.read<PageSelected>().changePageNumber(2.0);
+              Navigator.of(context).pop();
+              Navigator.of(context).pushNamed(FavoriteScreen.id);
+            },
           ),
           DetailedCard(
             icon: Icons.search,
             text: 'Search',
-            onTap: () {},
+            onTap: () {
+              context.read<PageSelected>().changePageNumber(4.0);
+              Navigator.of(context).pop();
+              Navigator.of(context).pushNamed(SearchScreen.id);
+            },
           ),
           DetailedCard(
             icon: Icons.shopping_cart,
             text: 'Shopping Cart',
-            onTap: () {},
+            onTap: () {
+              context.read<PageSelected>().changePageNumber(1.0);
+              Navigator.of(context).pop();
+              Navigator.of(context).pushNamed(CheckoutScreen.id);
+            },
           ),
           DetailedCard(
             icon: Icons.logout,
