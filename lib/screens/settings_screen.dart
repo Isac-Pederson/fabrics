e5a9f57ca24e1b10b521edc85ffe6cca4a7d93f8
Isac-Pederson <isacjped@gmail.com>
2023-09-12 16:42:08 -0700

    styling for setting screen

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,7 +1,10 @@
+import 'package:fabrics/components/constants/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
 import '../components/widgets/bottom_app_bar/rounded_bottom_app_bar.dart';
+import '../components/widgets/custom_button.dart';
 import '../models/provider/dark_mode.dart';
 
 class SettingScreen extends StatelessWidget {
@@ -14,17 +17,35 @@ class SettingScreen extends StatelessWidget {
       bottomNavigationBar: const RoundedBottomAppBar(),
       body: SafeArea(
           child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text('Email: logged_in_user@email.com'),
-          TextButton(
-            onPressed: () {},
-            child: const Text('Change Password'),
+          const Padding(
+            padding: EdgeInsets.only(bottom: 150),
+            child: Icon(
+              Icons.person_pin_circle_rounded,
+              size: 150,
+            ),
           ),
-          TextButton(
+          Center(
+            child: Text(
+              'Email: logged_in_user@email.com',
+              style: GoogleFonts.oswald(
+                fontSize: 20,
+              ),
+            ),
+          ),
+          CustomButton(
+            onPressed: () {},
+            text: 'Change Password',
+            color: kNavyBlue,
+          ),
+          CustomButton(
             onPressed: () {
               context.read<ToggleTheme>().isLightToggle();
             },
-            child: const Text('Toggle Light/Dark Mode'),
+            text: 'Toggle Light/Dark Mode',
+            color: kMidnightBlue,
           ),
         ],
       )),
